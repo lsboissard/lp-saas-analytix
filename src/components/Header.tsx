95e70d4890dc9a3a1bac9b874ba6309103161697
Leo Boissard <lsboissard@gmail.com>
2024-12-10 02:17:01 -0300

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,33 @@
  * Components
  */
 import { Button } from '@/components/ui/button';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import Logo from '@/components/Logo';
+
+/**
+ * Assets
+ */
+import { Menu } from 'lucide-react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className='h-16 grid grid-cols-1 items-center'>
+      <div className='container flex justify-between'>
+        <Logo variant='icon' />
+        <Button
+          variant='outline'
+          size='icon'
+          className='lg:hidden'
+        >
+          <Menu />
+        </Button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
