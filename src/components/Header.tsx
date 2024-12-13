03aca8c05c1798a4be22cc00ddf0b3b4614f186c
Leo Boissard <lsboissard@gmail.com>
2024-12-13 03:00:17 -0300

    add navmenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,17 @@ import {
 } from '@/components/ui/popover';
 import Logo from '@/components/Logo';
 import MobileMenu from '@/components/MobileMenu';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+  NavigationMenuViewport,
+} from '@/components/ui/navigation-menu';
 
 /**
  * Assets
@@ -25,6 +36,22 @@ function Header() {
     <header className='h-16 grid grid-cols-1 items-center md:h-20 lg:h-24'>
       <div className='container flex justify-between'>
         <Logo variant='icon' />
+
+        <NavigationMenu className='max-lg:hidden mx-auto'>
+          <NavigationMenuList>
+            {navMenu.map(({ href, label, submenu }, index) => (
+              <NavigationMenuItem key={index}>
+                <NavigationMenuLink
+                  href={href}
+                  className={navigationMenuTriggerStyle()}
+                >
+                  {label}
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+
         <Popover>
           <PopoverTrigger asChild>
             <Button
