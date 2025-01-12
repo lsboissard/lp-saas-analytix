f94226122bf1830cc9c3b6d5652b3922e0d62f25
Leo Boissard <lsboissard@gmail.com>
2025-01-11 21:47:58 -0300

    create initial style of hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,10 +16,10 @@ import { heroData } from '@/constants'
 
 function Hero() {
   return (
-    <section className="">
-      <div className="">
-        <div className="">
-          <p className="">
+    <section className="py-10 md:py-16">
+      <div className="container text-center">
+        <div className="max-w-screen-md mx-auto">
+          <p className="text-sm uppercase tracking-wider bg-secondary/50 text-secondary-foreground max-w-max mx-auto px-3 py-1 rounded-full border-t border-blue-500/10 backdrop-blur-3xl mb-6 md:mb-10">
             {heroData.sectionSubtitle}
           </p>
           <h2>
